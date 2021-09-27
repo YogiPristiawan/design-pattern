19f4e2a41ee01179cf82c51d721ff1e950c6a5c7
YogiPristiawan <yogistanada252@gmail.com>
2021-09-27 14:37:04 +0700

    rename a file, deleted unecessary file

--- FactoryMethod/example.php
+++ FactoryMethod/example.php
@@ -1,70 +1,13 @@
 <?php
-
-namespace RefactoringGuru\FactoryMethod\RealWorld;
-
-/**
- * The Creator declares a factory method that can be used as a substitution for
- * the direct constructor calls of products, for instance:
- *
- * - Before: $p = new FacebookConnector();
- * - After: $p = $this->getSocialNetwork;
- *
- * This allows changing the type of the product being created by
- * SocialNetworkPoster's subclasses.
- */
-abstract class SocialNetworkPoster
+interface Login
 {
-	/**
-	 * The actual factory method. Note that it returns the abstract connector.
-	 * This lets subclasses return any concrete connectors without breaking the
-	 * superclass' contract.
-	 */
-	abstract public function getSocialNetwork(): SocialNetworkConnector;
-
-	/**
-	 * When the factory method is used inside the Creator's business logic, the
-	 * subclasses may alter the logic indirectly by returning different types of
-	 * the connector from the factory method.
-	 */
-	public function post($content): void
-	{
-		// Call the factory method to create a Product object...
-		$network = $this->getSocialNetwork();
-		// ...then use it as you will.
-		$network->logIn();
-		$network->createPost($content);
-		$network->logout();
-	}
+	public function login(): string;
 }
 
-/**
- * This Concrete Creator supports Facebook. Remember that this class also
- * inherits the 'post' method from the parent class. Concrete Creators are the
- * classes that the Client actually uses.
- */
-class FacebookPoster extends SocialNetworkPoster
+class DoctorLogin implements Login
 {
-	private $login, $password;
-
-	public function __construct(string $login, string $password)
-	{
-		$this->login = $login;
-		$this->password = $password;
-	}
-
-	public function getSocialNetwork(): SocialNetworkConnector
-	{
-		// 
-		return new FacebookConnector($this->login, $this->password);
-	}
-}
-
-/**
- * This Concrete Creator supports LinkedIn.
- */
-class LinkedInPoster extends SocialNetworkPoster
-{
-	private $email, $password;
+	protected $email;
+	protected $password;
 
 	public function __construct(string $email, string $password)
 	{
@@ -72,60 +15,16 @@ class LinkedInPoster extends SocialNetworkPoster
 		$this->password = $password;
 	}
 
-	public function getSocialNetwork(): SocialNetworkConnector
+	public function login(): string
 	{
-		return new LinkedInConnector($this->email, $this->password);
+		return "DOCTOR logged in with $this->email and ( $this->password )";
 	}
 }
 
-/**
- * The Product interface declares behaviors of various types of products.
- */
-interface SocialNetworkConnector
+class PharmacyLogin implements Login
 {
-	public function logIn(): void;
-
-	public function logOut(): void;
-
-	public function createPost($content): void;
-}
-
-/**
- * This Concrete Product implements the Facebook API.
- */
-class FacebookConnector implements SocialNetworkConnector
-{
-	private $login, $password;
-
-	public function __construct(string $login, string $password)
-	{
-		$this->login = $login;
-		$this->password = $password;
-	}
-
-	public function logIn(): void
-	{
-		echo "Send HTTP API request to log in user $this->login with " .
-			"password $this->password\n";
-	}
-
-	public function logOut(): void
-	{
-		echo "Send HTTP API request to log out user $this->login\n";
-	}
-
-	public function createPost($content): void
-	{
-		echo "Send HTTP API requests to create a post in Facebook timeline.\n";
-	}
-}
-
-/**
- * This Concrete Product implements the LinkedIn API.
- */
-class LinkedInConnector implements SocialNetworkConnector
-{
-	private $email, $password;
+	protected $email;
+	protected $password;
 
 	public function __construct(string $email, string $password)
 	{
@@ -133,43 +32,67 @@ class LinkedInConnector implements SocialNetworkConnector
 		$this->password = $password;
 	}
 
-	public function logIn(): void
+	public function login(): string
 	{
-		echo "Send HTTP API request to log in user $this->email with " .
-			"password $this->password\n";
-	}
-
-	public function logOut(): void
-	{
-		echo "Send HTTP API request to log out user $this->email\n";
-	}
-
-	public function createPost($content): void
-	{
-		echo "Send HTTP API requests to create a post in LinkedIn timeline.\n";
+		return "PHARMACY logged in with $this->email and ( $this->password )";
 	}
 }
 
-/**
- * The client code can work with any subclass of SocialNetworkPoster since it
- * doesn't depend on concrete classes.
- */
-function clientCode(SocialNetworkPoster $creator)
+abstract class LoginCreator
 {
-	// ...
-	// $creator->post("Hello world!");
-	$creator->post("I had a large hamburger this morning!");
-	// ...
+	protected $email;
+	protected $password;
+
+	abstract function prepare(): Login;
+
+	public function doLogin()
+	{
+		$login = $this->prepare();
+		return $login->login();
+	}
 }
 
-/**
- * During the initialization phase, the app can decide which social network it
- * wants to work with, create an object of the proper subclass, and pass it to
- * the client code.
- */
-echo "Testing ConcreteCreator1:\n";
-clientCode(new FacebookPoster("john_smith", "******"));
-echo "\n\n";
+class Doctor extends LoginCreator
+{
 
-echo "Testing ConcreteCreator2:\n";
-clientCode(new LinkedInPoster("john_smith@example.com", "******"));
+	public function __construct(string $email, string $password)
+	{
+		$this->email = $email;
+		$this->password = $password;
+	}
+	public function prepare(): Login
+	{
+		return new DoctorLogin($this->email, $this->password);
+	}
+}
+
+class Pharmacy extends LoginCreator
+{
+
+	public function __construct(string $email, string $password)
+	{
+		$this->email = $email;
+		$this->password = $password;
+	}
+
+	public function prepare(): Login
+	{
+		return new PharmacyLogin($this->email, $this->password);
+	}
+}
+
+
+function main(LoginCreator $login)
+{
+	return $login->doLogin();
+}
+
+echo "Doctor do log in: \n";
+echo "================== \n";
+echo main(new Doctor("yogi.doctor@gmail.com", "password"));
+
+echo "\n\n\n";
+
+echo "Pharmacy do log in: \n";
+echo "================== \n";
+echo main(new Pharmacy("yogi.pharmacy@gmail.com", "password"));
